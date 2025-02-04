1722ee4ea24ab7cd23f1131036aad350741ab6a7
sweety singh <sweetysingh.1412@gmail.com>
2025-02-04 17:00:14 +0530

    Updated project with new features/fixes

--- src/components/ChatBotApp.jsx
+++ src/components/ChatBotApp.jsx
@@ -1,31 +1,32 @@
 import React, { useState } from "react";
-import './ChatBotApp.css';
-const ChatBotApp = ({onGoBack , chats ,setChats}) => {
-  const [inputValue , setInputValue] = useState('')
-  const [messages , setMessages] = useState(chats[0]?. messages ||[])
+import "./ChatBotApp.css";
+const ChatBotApp = ({ onGoBack, chats, setChats }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState(chats[0]?.messages || []);
+
+  
   const handleInputChange = (e) => {
-    setInputValue(e.target.value)
-  }
+    setInputValue(e.target.value);
+  };
   const sendMessage = () => {
-    if(inputValue.trim === '') return
+    if (inputValue.trim === "") return;
     const newMessage = {
-      type: 'prompt',
+      type: "prompt",
       text: inputVlaue,
-      timestamp:new Date().toLocaleTimeString(),
+      timestamp: new Date().toLocaleTimeString(),
+    };
+    const updatedMessages = [...messages, newMessage];
+    setMessages(updatedMessages);
+    setInputValue("");
 
-    }
-    const updateMessages = [...messages , newMessage]
-    setMessages(updateMessages)
-    setInputValue('')
-
-    const updateChats = chats.map((chat , index) =>{
-      if(index === 0){
-        return {...chats , messages:updateMessages}
+    const updatedChats = chats.map((chat, index) => {
+      if (index === 0) {
+        return { ...chats, messages: updatedMessages };
       }
-      return chat
-    })
-    setChats(updateChats)
-  }
+      return chat;
+    });
+    setChats(updatedChats);
+  };
   return (
     <div className="chat-app">
       <div className="chat-list">
@@ -33,6 +34,16 @@ const ChatBotApp = ({onGoBack , chats ,setChats}) => {
           <h2>Chat List</h2>
           <i className="bx bx-edit-alt new-chat"></i>
         </div>
+        {chats.map((chat, index) => (
+          <div
+            key={index}
+            className={`chat-list-item ${index === 0 ? "active" : ""}`}
+          >
+            <h4>{chat.id}</h4>
+            <i className="bx bx-x-circle"></i>
+          </div>
+        ))}
+
         <div className="chat-list-item active">
           <h4>chat 28/01/2025 3:17:30pm</h4>
           <i className="bx bx-x-circle"></i>
@@ -60,13 +71,18 @@ const ChatBotApp = ({onGoBack , chats ,setChats}) => {
           <div className="response">
             Hello! I'm just a computer program , so , I don't have feelings, but
             I'm here and ready to assist you? How can I help you today?
-            <span>3:17:30pm</span></div>
+            <span>3:17:30pm</span>
+          </div>
           <div className="typing">Typing</div>
         </div>
         <form className="msg-form">
-        <i className="fa-solid fa-face-smile emoji"></i>
-        <input type="text" className="msg-input"  placeholder="Type a message...."/>
-        <i className="fa-solid fa-paper-plane"></i>
+          <i className="fa-solid fa-face-smile emoji"></i>
+          <input
+            type="text"
+            className="msg-input"
+            placeholder="Type a message...."
+          />
+          <i className="fa-solid fa-paper-plane"></i>
         </form>
       </div>
     </div>
